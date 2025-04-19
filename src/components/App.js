715f352153ac610d065ed8ed272554a8ba7285d4
debayudh2001 <debayudh31@gmail.com>
2025-04-19 16:43:46 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,64 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useCallback, useMemo, useRef, useState } from "react";
 
+const App = () => {
+    let email = useRef(null)
+    let password = useRef(null)
+    const [error, setError] = useState({
+        email: "",
+        password: ""
+    })
+
+    let userCred = useMemo(() => {
+        return { email: "dev@gmail.com", password: "dev1234" }
+    }, [])
+
+    function validateForm(email, password) {
+        let valid = true
+        if (userCred.email !== email) {
+            setError(prev => {
+                return { ...prev, email: "User not found" }
+            })
+            valid = false
+        }
+        if (userCred.password !== password) {
+            setError(prev => {
+                return { ...prev, password: "Password Incorrect" }
+            })
+            valid = false
+        }
+        return valid
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        setTimeout(() => {
+            if (validateForm(email.current.value, password.current.value)) {
+                setError({
+                    email: "",
+                    password: ""
+                })
+                console.log("Logged In Successfully")
+            }
+        }, 3000)
+    }
+
+    //console.log(error)
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input id="input-email" ref={email} type="text" placeholder="Enter your email" />
+                <br />
+                {error.email && <span id="user-error" style={{ color: "red" }}>{error.email}</span>}
+                <br />
+                <input id="input-password" ref={password} type="password" placeholder="Enter your password" />
+                <br />
+                {error.password && <span id="password-error" style={{ color: "red" }}>{error.password}</span>}
+                <br />
+                <button id="submit-form-btn" type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default App
